2986351bfb2a2051bd0ddb0055f4a37f07eb78bb
Sven Oliver Moll <svolli@svolli.de>
2019-04-13 23:28:45 +0200

    v3s: set up for configurations for LicheePi Zero - with and without dock - with and without 800x480 LCD every combination now has its own defconfig

--- v3s/board/licheepi-zero/boot-dock.cmd
+++ v3s/board/licheepi-zero/boot-dock.cmd
@@ -2,6 +2,6 @@ setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 
 mmc dev 0
 fatload mmc 0 $kernel_addr_r zImage
-fatload mmc 0 $fdt_addr_r sun8i-v3s-licheepi-zero.dtb
+fatload mmc 0 $fdt_addr_r sun8i-v3s-licheepi-zero-dock.dtb
 
 bootz $kernel_addr_r - $fdt_addr_r
--- v3s/package/most/most.mk
+++ v3s/package/most/most.mk
@@ -6,14 +6,14 @@
 
 MOST_SITE = http://www.jedsoft.org/releases/most
 MOST_VERSION = 5.1.0
-MOST_SOURCE = most-$(MOST_VERSION).tar.gz
-MOST_LICENSE = GPL-2.0
-MOST_LICENSE_FILES = COPYING
+MOST_LICENSE = GPL-2.0+
+MOST_LICENSE_FILES = COPYING COPYRIGHT
+MOST_DEPENDENCIES = slang
 
-define MOST_BUILD_CMDS
-	$(SED) 's/ slangversion / /g' $(@D)/src/Makefile
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+define MOST_REMOVE_LOCAL_SLANG_CHECK
+	$(SED) 's/ slangversion / /g' $(@D)/src/Makefile.in
 endef
+MOST_POST_PATCH_HOOKS += MOST_REMOVE_LOCAL_SLANG_CHECK
 
 define MOST_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/src/objs/most $(TARGET_DIR)/usr/bin/most
